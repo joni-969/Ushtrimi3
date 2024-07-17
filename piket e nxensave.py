27e5fbc89d3f65110af540b1db34c90ad158fdc5
JonKadriu <jon.f.kadriu@gmail.com>
2024-07-17 11:13:30 +0200

    Add files via upload

---
+++ piket e nxensave.py
@@ -0,0 +1,8 @@
+piket = {
+    "joni" : 56,
+    "olti" : 54,
+    "alpi" :19,
+}
+shumapikave = sum(piket.values())
+piketmesatare = shumapikave / len(piket)
+print(f"Piket mesatare jane {piketmesatare}")
